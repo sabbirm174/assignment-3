73f649ad9d2d710f4a349e666706e8e1fdf30b85
Sabbir Alam <sabbirm174@gmail.com>
2021-01-22 16:27:10 +0600

    remove es6 featur

--- assignment.js
+++ assignment.js
@@ -2,7 +2,7 @@
 
 // ---------------- start-kilometerToMeter ---------------- //
 function kilometerToMeter(km) {
-    let met = 1;
+    var met = 1;
     if (km >= 0) {
         met = 1000 * km;      // 1 km = 1000m
     } else {
@@ -14,12 +14,12 @@ function kilometerToMeter(km) {
 
 // ---------------- start-budgetCalculator ---------------- //
 function budgetCalculator(watch, phone, laptop) {
-    let totalPrice = 0;
+    var totalPrice = 0;
     if(watch === parseInt(watch) && phone === parseInt(phone) && laptop === parseInt(laptop)){
         if (watch >= 0 && phone >= 0 && laptop >= 0) {
-            const watchPrice = watch * 50;       //watch price 50
-            const phonePrice = phone * 100;     //phone price 100
-            const laptopPrice = laptop * 500;    //laptop price 500
+            var watchPrice = watch * 50;       //watch price 50
+            var phonePrice = phone * 100;     //phone price 100
+            var laptopPrice = laptop * 500;    //laptop price 500
             totalPrice = watchPrice + phonePrice + laptopPrice;
         } else {
             return " input must be positive value"
@@ -35,20 +35,20 @@ function budgetCalculator(watch, phone, laptop) {
 // ---------------- Start-hotelCost ---------------- //
 
 function hotelCost(days) {
-    let totalCost = 0;
+    var totalCost = 0;
     if (days >= 0 && days === parseInt(days)) {
         if (days <= 10) {               // first ten days
             totalCost = days * 100;     // first ten days result
         } else if (days <= 20) {         // second ten days
-            const firstTen = 10 * 100;
-            const remaining = days - 10;
-            const secondTen = remaining * 80;
+            var firstTen = 10 * 100;
+            var remaining = days - 10;
+            var secondTen = remaining * 80;
             totalCost = firstTen + secondTen;   // second ten days result
         } else { // other days
-            const firstTen = 10 * 100;
-            const secondTen = 10 * 80;
-            const remaining = days - 20;
-            const otherDays = remaining * 50;
+            var firstTen = 10 * 100;
+            var secondTen = 10 * 80;
+            var remaining = days - 20;
+            var otherDays = remaining * 50;
             totalCost = firstTen + secondTen + otherDays;   // other days result
         }
     } else {
@@ -60,8 +60,8 @@ function hotelCost(days) {
 
 // ---------------- Start-megaFriend ---------------- //
 function megaFriend(arr) {
-    let arrLen = 0;
-    let result;
+    var arrLen = 0;
+    var result;
     if(Array.isArray(arr)){         // input value must be an array
         if (arr.length != 0) {      // if array valule is not empty
             for (var i = 0; i < arr.length; i++) {
